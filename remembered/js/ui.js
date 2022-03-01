4b629af6bb9df3138e95a0506fbf5b276daa71ad
lhn1208 <lhn1208@naver.com>
2022-03-01 14:38:12 +0900

    no message

--- remembered/js/ui.js
+++ remembered/js/ui.js
@@ -1,14 +1,25 @@
+
 $(document).ready(function () {
+    var gnb=$('.remember_gnb');
     $(window).scroll(function(){
         //gnb
         var scTop = $(window).scrollTop();
-        var gnb=$('.remember_gnb');
         if(scTop>0){							
             gnb.addClass('fix_on');						
         }else{
             gnb.removeClass('fix_on');
         }           
     });
+    //mobile menu
+    gnb.find('.menu').click(function(){
+        $('body').addClass('fixed');
+        $('.mobile_menu').addClass('on');
+    })
+    $('.mobile_menu .close').click(function(){
+        $('body').removeClass('fixed');
+        $('.mobile_menu').removeClass('on');
+    })
+    //공개여부
     $('.message .switch').on({
         click:function(e){
             $('.message .switch').toggleClass('active');
