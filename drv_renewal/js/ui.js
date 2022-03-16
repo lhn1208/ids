c9841732833b2f2bb5ad45bae684ea234b518905
10-N3277\user <lhn1208@idstrust.com>
2022-03-16 11:24:21 +0900

    닥터빌리뉴얼

--- drv_renewal/js/ui.js
+++ drv_renewal/js/ui.js
@@ -1,52 +1,66 @@
-
 window.onload = function () {
-    let els = document.querySelector(".all_menu_btn");
-    els.style.display = "none";
-    alert('els' + els);
 }
 
+//전체메뉴
+var allMenu;
+function allmenu_fnc(){
+    allMenu = document.getElementById("all_menu_box");
+    if(!allMenu.classList.contains('active')){
+        allMenu.classList.add('active');
+    }else{
+        allMenu.classList.remove('active');
+    }      
+}
 
-(function ($) {
-    $(document).ready(function () {
-        //header
-    //    var $gnb_area= $('.drv_gnb_area');
-   
-    //     var $gnb=$('.drv_gnb_list>li');
-    //     $gnb.mouseenter(function(){
-    //         $(this).children('.drv_gnb_sub').stop().slideDown();;
-    //     })
-    //     $gnb.mouseleave(function(){
-    //         $(this).children('.drv_gnb_sub').stop().slideUp();
-    //     })
+function allmenu_close(){
+    allMenu.classList.remove('active');
+}
 
-    // $('.drv_gnb_sub').parent().hover(function(){
-    //     var submenu=$(this).children('.drv_gnb_sub');
-    //     if ( $(submenu).is(':hidden') ) {
-    //         $(submenu).slideDown(200);
-    //       } else {
-    //         $(submenu).slideUp(200);
-    //       }
-    // });
-        
-        //전체메뉴
-         var $all_menu_box= $('.all_menu_box');
-         var $menu_close_btn=$all_menu_box.find('.btn_close');
-         var $menue_btn=$('.all_menu_btn');
-        $($menue_btn).click(function(){
-            if($(this).hasClass('active')){                        
-                $(this).removeClass('active');
-                $all_menu_box.removeClass('active');
-            }else{
-                $(this).addClass('active');
-                $all_menu_box.addClass('active');
-            }
-        })
-        $($menu_close_btn).click(function(){
-            $menue_btn.removeClass('active');
-            $all_menu_box.removeClass('active');
-        });
+// footer
+var familySite;
+function family_fun(){
+    familySite = document.getElementById("family_site");
+    if(!familySite.classList.contains('active')){
+        familySite.classList.add('active');
+    }else{
+        familySite.classList.remove('active');
+    }   
+}
+var foo_notice_swiper = new Swiper(".drv_footer .notice_slide", {
+    slidesPerView: 1,
+    simulateTouch:false,
+    navigation: {
+        nextEl: ".drv_footer .notice_btn .swiper-button-next",
+        prevEl: ".drv_footer .notice_btn .swiper-button-prev",
+    },
+});
 
+// document.getElementById("allmenu_close").addEventListener('click', allmenu_close);
+// function allmenu_close(){
+    // allMenu.style.display = 'block'; 
+
+// }
 
 
-    });
-})(jQuery);
+//         //전체메뉴
+//          var $all_menu_box= $('.all_menu_box');
+//          var $menu_close_btn=$all_menu_box.find('.btn_close');
+//          var $menue_btn=$('.all_menu_btn');
+//         $($menue_btn).click(function(){
+//             if($(this).hasClass('active')){                        
+//                 $(this).removeClass('active');
+//                 $all_menu_box.removeClass('active');
+//             }else{
+//                 $(this).addClass('active');
+//                 $all_menu_box.addClass('active');
+//             }
+//         })
+//         $($menu_close_btn).click(function(){
+//             $menue_btn.removeClass('active');
+//             $all_menu_box.removeClass('active');
+//         });
+
+
+
+//     });
+// })(jQuery);
